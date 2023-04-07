8cf04ff4684273e94fa63603fd128211c707813f
camilobit <camiloacevedovargas@gmail.com>
2023-04-07 13:16:32 -0500

    commit ejercico 08 de react opcional

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
@@ -5,8 +5,15 @@ export default class Animals extends React.Component {
   constructor(props) {
     super();
   }
-
   render() {
-    return <div></div>
+    return  <div>
+              {Animals.animals.map((animal) => (
+                <div key={animal.id}>
+                  <h5>{animal.name}</h5>
+                  <img src={animal.image} alt={animal.name} width="300px" />
+                  <span>{animal.species}</span>
+                </div>
+              ))}
+            </div>
   }
 }
--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -1,17 +1,53 @@
 import React from "react";
-// eslint-disable-next-line no-unused-vars
+//eslint-disable-next-line no-unused-vars
 import Animals from "../Animals/Animals";
-// eslint-disable-next-line no-unused-vars
+//eslint-disable-next-line no-unused-vars
 import Species from "../Species/Species";
-// import styledZoo from "./Zoo.module.css";
+import styledZoo from "./Zoo.module.css";
 
 export default function Zoo() {
   /* Escribe acá tu código */
+  const handleInputChange = (event) => {
+    setZoo({ ...zoo, zooName: event.target.value });
+  };
+
+  const handleSpecies = (e) => {
+    setZoo({ ...zoo, species: e.target.value });
+  };
+
+  const handleAllSpecies = () => {};
+
+
+  const [zoo, setZoo] = React.useState({
+    zooName: "",
+    animals: [],
+    species: [],
+    allAnimals: [],
+  })
+
+
+  React.useEffect (() => {
+    fetch('http://localhost:3001/zoo')
+  .then((res) => res.json())
+  .then((data) =>
+    setZoo({
+        ...zoo,
+        animals: data.animals,
+        species: data.species,
+        allAnimals: data.animals,
+    })
+  )
+  .catch((error) => console.log(error));})
+    
 
 
   return (
     <div>
-      <h1>-</h1>
+    <label htmlFor="zooName">Zoo Name:</label>
+    <input id="zooname" type="text" onChange={handleInputChange} value={zoo.zooName} ></input>
+      <h1>{zoo.zooName}</h1>
+      <Species species={zoo.species} handleSpecies={handleSpecies} handleAllSpecies={handleAllSpecies} />
+        <Animals animals={zoo.animals} />
     </div>
   );
 }
