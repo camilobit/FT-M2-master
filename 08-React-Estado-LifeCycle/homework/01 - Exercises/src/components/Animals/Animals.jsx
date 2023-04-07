8b7fd4e09e33e8c063e5d3a35a9b979d31649221
camilobit <camiloacevedovargas@gmail.com>
2023-04-07 13:17:38 -0500

    commit ejercico 08 de react opcional

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import styledAnimals from './Animals.module.css'
+import styledAnimals from './Animals.module.css'
 
 export default class Animals extends React.Component {
   constructor(props) {
